d7f88629b32cba1a0158fea1ecb8adbee0cf3e54
sebaspasker <sebaspasker@gmail.com>
2022-05-06 11:58:17 +0200

    Búsqueda en base al MMSI y conversión a un GEOJson de forma embebida

---
+++ webgis/webgisapp/utils/ais_to_geojson.py
@@ -0,0 +1,29 @@
+from webgisapp.models import AISVessel, Vessel
+from geojson import FeatureCollection, Feature, LineString
+
+def AISQuery_To_LineStringCollection(Vessels, AISQuery):
+    """ 
+    From a Vessel and AISVessel (Model) QuerySet creates
+    a GeoJSON LineStringCollection
+
+    Parameters:
+        (QuerySet, QuerySet)
+        returns FeatureCollection
+    """
+    features = []
+    for Vessel in Vessels:
+        AIS_Q = AISQuery.filter(MMSI=Vessel)
+        if len(AIS_Q) > 0:
+            features.append(
+                Feature(
+                    geometry = LineString([(AIS.LON, AIS.LAT) for AIS in AIS_Q]),
+                    properties = {
+                        'MMSI': Vessel.MMSI,
+                        'VesselName' : Vessel.VesselName,
+                        'Matricula' : Vessel.Matricula
+                    }
+                )
+            )
+    return FeatureCollection(features)
+
+print(help(AISQuery_To_LineStringCollection))
--- webgis/webgisapp/views.py
+++ webgis/webgisapp/views.py
@@ -1,34 +1,40 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, Http404, HttpResponseNotFound
-from django.template import loader
-from webgisapp.models import AISVessel, Vessel
-from django.db.models import Q
 from .forms import SearchForm
+from geojson import FeatureCollection
+from webgisapp.models import AISVessel, Vessel
+from webgisapp.utils.ais_to_geojson import AISQuery_To_LineStringCollection
 
 
+# Mapa de calor 
 def index(request):
     return render(request, 'webgisapp/index.html')
 
+# Mapa de rutas de tres rutas mmsi ejemplo
 def maproute(request):
     return render(request, 'webgisapp/maproute.html')
     
+# Mapa de rutas con filtrado y popups
 def maproutelayer(request):
     return render(request, 'webgisapp/maproute_layer.html')
 
+# Búsqueda de rutas en base al MMSI
 def search(request):
     if request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid():
             mmsi = form.cleaned_data['MMSI']
             try:
-                v = Vessel.objects.get(MMSI__contains=mmsi)
-                ais = AISVessel.objects.filter(MMSI=v)
-                return HttpResponse("<h4> Encontrado " + ais[0].to_string() + ais[1].to_string() +  "</h4>") 
+                v = Vessel.objects.filter(MMSI__contains=mmsi)
+                ais = AISVessel.objects.filter(MMSI__in=v)
+                collection = AISQuery_To_LineStringCollection(v, ais)
+                return render(request, 'webgisapp/maproute_search.html', {'collection' : collection})
             except Exception as e:
                 raise e
     else:
         form = SearchForm()
         return render(request, 'webgisapp/form.html', {'form' : form})
 
+# Búsqueda del MMSI pero desde el frontend (no busca en la base de datos) 
 def maproutefilter(request):
     return render(request, 'webgisapp/maproute_filter.html')
